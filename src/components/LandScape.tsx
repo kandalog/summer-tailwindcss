9759233acbe9c2be92b4797ff680b16eec96c535
kandalog <asuasu.tee@gmail.com>
2025-05-11 14:02:09 +0900

    landscape作成

--- src/components/LandScape.tsx
+++ src/components/LandScape.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
 import React from "react";
 
 function LandScape() {
-  return <div>LandScape</div>;
+  return (
+    <div className="md:flex">
+      <div>
+        <Image
+          src="/assets/landscape1.png"
+          width={3000}
+          height={3000}
+          alt="img1"
+        />
+      </div>
+      <div className="hidden md:block">
+        <Image
+          src="/assets/landscape2.png"
+          width={3000}
+          height={3000}
+          alt="img2"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LandScape;
